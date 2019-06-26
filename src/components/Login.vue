4ff7ea3580ec8f92eebf067ef65cf1978297fe29
huna <huna@wemakeprice.com>
2019-06-26 15:11:25 +0900

    Added the Login component

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    <h2>Sign in</h2>
+    <form class="form-group">
+      <input class="form-control" type="email" placeholder="Email adderss" autocomplete="username">
+      <input class="form-control" type="password" placeholder="Password" autocomplete="current-password">
+      <button class="btn btn-primary">Sign in</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    console.log('login')
+  }
+}
+</script>
