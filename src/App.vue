d9ce40d2425f6cb854c217ddcc459ce963b9b522
huna <huna@wemakeprice.com>
2019-06-27 10:34:37 +0900

    Dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
     <img class="logo" src="./assets/logo.png">
-    <!-- <login></login> -->
-    <register></register>
-    <!-- <stories></stories> -->
+    <component :is="currentComponent"></component>
+    <hr>
+    <div class="btn-group btn-block">
+      <button @click="currentComponent = 'login'" class="btn btn-success">Login</button>
+      <button @click="currentComponent = 'register'" class="btn btn-primary">Register</button>
+      <button @click="currentComponent = 'stories'" class="btn btn-info">Stories</button>
+    </div>
   </div>
 </template>
 
 <script>
-// import Login from './components/Login'
+import Login from './components/Login'
 import Register from './components/Register'
-// import Stories from './components/Stories'
+import Stories from './components/Stories'
 
 export default {
   name: 'App',
   components: {
-    // Login
-    Register
-    // Stories
+    Login,
+    Register,
+    Stories
+  },
+  data () {
+    return {
+      currentComponent: 'login'
+    }
   }
 }
 </script>
